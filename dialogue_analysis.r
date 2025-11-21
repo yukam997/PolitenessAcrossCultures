721de6456858ba07bab2616a6df3502279914457
Yuka <myuhka12@gmail.com>
2025-11-20 18:03:09 -0800

    analysis code

--- dialogue_analysis.r
+++ dialogue_analysis.r
@@ -1,13 +1,20 @@
 # Setup 
+install.packages("lme4")
+install.packages("lmerTest")
 library(lme4)
 library(lmerTest)
 library(tidyverse)
 library(DT)
+install.packages("simr")
 library(simr)
 library(ggeffects)
 library(cowplot)
 library(broom.mixed)
-library(MuMIn)
+install.packages("pwr")
+library(pwr)
+
+
+
 
 # Data Loading 
 df <- read.csv("data/combined.csv")
@@ -30,20 +37,21 @@ base_model <- lmer(interpretation_difference ~
            (1|person_id) + (1|scenario), 
            data = df)
 summary(base_model)
-interaction_model <- lmer(interpretation_difference ~ 
+interaction_model_l4 <- lme4::lmer(interpretation_difference ~ 
            country * modifier + predicate +
            (1|person_id) + (1|scenario), 
            data = df)
+
+
 summary(interaction_model)
 #model allowing for different effects for each modifier provided a significantly better f it (χ2(4) = 14.48,p = .006)
 anova(base_model, interaction_model)
-
+cohens_f(comparison)
+library(effectsize)
 library(emmeans)
 # “quite” was interpreted as weaker by British participants compared to Americans (β = −0.28,SE = 0.12,p = .023); “very” was interpreted as stronger by British participants (β = 0.28,SE = 0.13,p = .032),
 emmeans_results <- emmeans(interaction_model, pairwise ~ country | modifier)
 emmeans_results
-
-
 
 
 # Valence analysis
@@ -81,3 +89,21 @@ valence_country_model <- lmer(interpretation_difference ~
 emmeans_valence <- emmeans(valence_country_model, pairwise ~ country | modifier | valence)
 # British participants' weaker interpretation of ``quite'' occurred only for negative predicates ($p = .022$), while their stronger interpretation of ``very'' was most pronounced with positive predicates ($p = .053$).
 print(emmeans_valence)
+
+# power simulations
+install.packages("MuMIn")
+library(MuMIn)
+getCall(interaction_model_l4)
+r.squaredGLMM(interaction_model)
+r.squaredGLMM(base_model)
+compare(. ~country + modifier + predicate +
+          (1|person_id) + (1|scenario))(interaction_model)
+rcompare(~ country + modifier + predicate +
+           (1|person_id) + (1|scenario),)(interaction_model)
+powerSim(interaction_model_l4,
+         test = anova,
+         fixed("country:modifier"))
+powerSim(interaction_model, ~country + modifier + predicate +
+           (1|person_id) + (1|scenario))
+
+pwr.anova.test(k=2,f=0.4,sig.level=0.05,power=0.9)
